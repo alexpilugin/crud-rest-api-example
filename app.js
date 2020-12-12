c8a9eac2274f4a98c959401603d4b5239c6c1bbe
Alex Pilugin <alex.pilugin@microsec.co.uk>
2020-12-12 02:21:24 +0000

    loading with delay + spinner

--- app.js
+++ app.js
@@ -2,11 +2,21 @@ const express = require('express')
 const app = express()
 const path = require('path')
 
+// Our db on a serverside:
+const CONTACTS = [
+  {id: 1, name: 'Default Contact', value: '+44-0000-000-0000', marked: false}
+]
 
-
+// GET
+app.get('/api/contacts', (req, res) => {
+  setTimeout( () => {
+    res.status(200).json(CONTACTS)
+  }, 1000)
+})
 
 app.use(express.static(path.resolve(__dirname, 'client')))
 
+// GET
 app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
 })
--- client/frontend.js
+++ client/frontend.js
@@ -1,5 +1,15 @@
 import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.esm.browser.js';
 
+Vue.component('loader', {
+  template: `
+    <div style="display: flex; justify-content: center; align-items: center;">
+      <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+    </div>
+  `
+})
+
 new Vue({
   el: '#app',
   data() {
@@ -8,9 +18,15 @@ new Vue({
         name: '',
         value: ''
       },
-      contacts: []
+      contacts: [],
+      loading: false
     }
   }, 
+  async mounted() {
+    this.loading = true
+    this.contacts = await request('/api/contacts')
+    this.loading = false
+  },
   computed: {
     canCreate() {
       return this.form.name.trim() && this.form.value.trim()
@@ -32,3 +48,22 @@ new Vue({
     }
   }
 })
+
+async function request(url, method = 'GET', data = null) {
+  try {
+    const headers = {}
+    let body
+    if (data) {
+      headers['Content-Type'] = 'application/json',
+      body = JSON.stringify(data)
+    }
+    const response = await fetch(url, {
+      method,
+      headers,
+      body
+    })
+    return await response.json()
+  } catch (e) {
+    console.error(e.message)
+  }
+}
